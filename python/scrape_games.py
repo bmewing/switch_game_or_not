f17d9f2a38c8243586421a5fd1a027b12d441a12
Mark Ewing <mewing@eastman.com>
2018-12-17 15:46:25 -0500

    adding a ton of files

--- python/scrape_games.py
+++ python/scrape_games.py
@@ -3,30 +3,38 @@ import json
 import csv
 import functools
 import time
+import re
+import math
+from datetime import datetime
 
 
-def extract_game(g, k):
+odate = datetime(2017, 3, 2, 0, 0)
+thisweek = math.floor((datetime.today() - odate).days / 7)
+
+
+def extract_game(g):
     try:
-        if g['title'] in k:
-            print("Found existing game!")
-            return 1
-        else:
-            with open('switch_games.csv', 'a', newline='') as csvfile:
-                fields = ['title', 'release']
-                writer = csv.DictWriter(csvfile, fieldnames=fields)
-                writer.writerow({
-                                 'title': g['title'],
-                                 'release': g['release_date']
-                                 })
-            return 0
+        fields = ['title', 'release', 'week']
+        rdate = datetime.strptime(g['release_date'], '%b %d, %Y')
+        odate = datetime(2017, 3, 2, 0, 0)
+        week = math.floor((rdate - odate).days / 7)
+        release = str(rdate)[:10]
+        title = re.sub('"', "'", g['title'])
+        title = re.sub("&[^ ]*?;", "", title)
+        title = re.sub("é", "e", title)
+        title = re.sub("®", "", title)
+        title = re.sub("Û", "U", title)
+        with open('switch_games_week_{}.csv'.format(str(week)), 'a', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fields)
+            writer.writerow({
+                             'title': title,
+                             'release': release,
+                             'week': week
+                             })
+        return 0
     except KeyError:
         return 1
 
-
-known = []
-with open('switch_games.csv', newline='') as csvf:
-    reader = csv.DictReader(csvf)
-    known = [r['title'] for r in reader]
 
 u = "https://www.nintendo.com/json/content/get/filter/game?limit=40&offset={0}&system=switch&sort=release&direction=des"
 go = True
@@ -35,7 +43,7 @@ while go:
     page = requests.get(u.format(str(i*40)))
     data = json.loads(page.content)
     games = data['games']['game']
-    res = [extract_game(g, known) for g in games]
+    res = [extract_game(g) for g in games]
     go = functools.reduce(lambda a, b: a+b, res) == 0
     i += 1
     time.sleep(3)
