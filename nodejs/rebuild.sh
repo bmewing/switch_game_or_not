bb4eb63bbe8f2a20d8b9db9bd020ffad39fa174c
Mark Ewing <b.mark@ewingsonline.com>
2018-12-19 21:36:08 +0000

    changed order of rebuild

--- nodejs/rebuild.sh
+++ nodejs/rebuild.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
+./node_modules/.bin/babel src --out-dir ./
+docker build -t pofigster/switch_or_not .
 for f in $(docker ps -aqf "name=epic_heisenberg")
 do
     docker stop $f
     docker rm $f
 done
-./node_modules/.bin/babel src --out-dir ./
-docker build -t pofigster/switch_or_not .
 docker run --name "epic_heisenberg" -p 80:8080 -d pofigster/switch_or_not 
 docker container ls
